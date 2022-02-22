d247a248f33d4c794bd4e5cd63b5cca6067e31b1
sammy ocharo <obanyisammy@gmail.com>
2022-02-22 17:01:23 +0300

    v1

--- webBackend/settings.py
+++ webBackend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-5(lux9+u9#4plul-9quff6(zr-3f(9a3md#=r09x&4b@x&ga2_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
